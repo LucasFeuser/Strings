f91951577c66785a8cd3c6b833eaf12f14c1c7a3
LucasFeuser <lamentavelmentemente@outlook.com>
2022-08-23 13:56:46 -0300

    Module Strings Course balta.io

---
+++ StringComparation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringComparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Clear();
+           var text = "Test";
+           var text2 = "This text is a test";
+           //Case sensitive = T != t;
+           Console.WriteLine(text.CompareTo("Test"));
+           Console.WriteLine(text.CompareTo("test"));
+
+           Console.WriteLine(text2.Contains("test"));//Verify if var text2, contain "test" defined in u value;
+           Console.WriteLine(text2.Contains("Test"));//In this case, test set a T Upper, remember C# is a case sensitive;
+
+        }
+    }
+}
---
+++ StringInterpol/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringInterpol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Insert value in String / Mix String to otter values.
+        Console.Clear();
+        var price = 10.2 + 9; //Price is double
+        var text = "O preço do produto é " + price + " este valor é apenas na promoção \n" ; //This process insert + call is interpol.
+
+        var buy = true;
+
+        var stringFor = string.Format("O preço do produto é {0} apenas na promoção {1} \n", true, price);
+        //In this case, need to add in final of the string two values, or after compile program, show a exception //Use to format string / Insert value into string.
+
+        var StringCifroni = $@"O preço do produto é {price}"; //Add @ ignore special character, ou $ to add other value type. \n = line break;
+
+        Console.WriteLine(text);
+        Console.WriteLine(buy + "\n" );
+        Console.WriteLine(stringFor);
+        Console.WriteLine(StringCifroni);
+        }
+    }
+}
